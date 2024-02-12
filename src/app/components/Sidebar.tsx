e3a44711874ace2086f820be1a736846a53d3f4f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-12 07:24:50 -0400

    Se agregó el nuevo componente de pokemon y se corrigió el largo de sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
 import { SidebarMenuItem } from './SidebarMenuItem'
+import { TbPokeball } from 'react-icons/tb'
 
 const menuItems = [
     {
@@ -16,11 +17,17 @@ const menuItems = [
       title: 'Counter',
       subTitle: 'Contador Client Side'
     },
+    {
+      path: '/dashboard/pokemons',
+      icon: <TbPokeball size={40} />,
+      title: 'Pokemons',
+      subTitle: 'Generación Estática'
+    },
   ]
 
 export const Sidebar = () => {
   return (
-    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
         <div id="logo" className="my-4 px-6">
             <h1 className="flex items-center  text-lg md:text-2xl font-bold text-white">
                 <IoLogoReact className='mr-2' />
