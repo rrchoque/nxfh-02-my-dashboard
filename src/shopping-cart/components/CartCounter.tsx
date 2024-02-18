2c1a2aa4777a10dee318c468c56dfef7e7d5eba9
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-18 08:03:50 -0400

    Se implementó redux en el módulo de counter

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, initCounterState, substractOne } from '@/store/counter/counterSlice';
+import { useEffect } from 'react';
 
 interface Props {
   value?: number;
@@ -8,7 +10,13 @@ interface Props {
 
 export const CartCounter = ({  value = 0 }: Props) => {
 
-  const [count, setCount] = useState(value);
+  //const [count, setCount] = useState(value);
+  const count = useAppSelector( state => state.counter.count );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initCounterState(value))
+  }, [dispatch, value])
 
   return (
     <>
@@ -16,13 +24,13 @@ export const CartCounter = ({  value = 0 }: Props) => {
 
       <div className="flex">
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch(addOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
         </button>
 
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch(substractOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           -1
         </button>
