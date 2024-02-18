0ebed743a53ba39c542e6906712e1ff0329b06d9
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-18 08:30:17 -0400

    Se implementó redux en el módulo main del dashboard

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,4 +1,4 @@
-import { SimpleWidget } from "@/components";
+import { WidgetsGrid } from "@/components";
 
 export const metadata = {
   title: 'Admin Dashboard',
@@ -11,11 +11,7 @@ export const metadata = {
        <h1 className="mt-2 text-3xl">Dashboard</h1>
        <span className="text-xl">Información general</span>
        <div className="flex flex-wrap p-2 items-center justify-center" >
-        <SimpleWidget />
-
-        <SimpleWidget />
-        <SimpleWidget />
-        <SimpleWidget />
+        <WidgetsGrid/>
        </div>
      </div>
    );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './sidebar/Sidebar';
 
-export { SimpleWidget } from './dashboard/SimpleWidget'
+export { SimpleWidget } from './dashboard/SimpleWidget';
+export { WidgetsGrid } from './dashboard/WidgetsGrid';
