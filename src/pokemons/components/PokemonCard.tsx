6fc351d1746887139f5fadb9902542839c67b8bf
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-02-19 07:34:06 -0400

    Se creó el state de pokemons para manejar favoritos

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,17 +1,21 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 import { SimplePokemon } from "../interfaces/simple-pokemon";
-import { IoHeartOutline } from "react-icons/io5";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import { useAppSelector } from "@/store";
 
 interface Props {
   pokemon: SimplePokemon;
 }
 
-
 export const PokemonCard = ({ pokemon }:Props) => {
 
   const { id, name } = pokemon;
+  const isFavorite = useAppSelector( state => !!state.pokemons[id] );
 
+  console.log(isFavorite)
 
   return (
     <div className="mx-auto right-0 mt-2 w-60">
@@ -42,11 +46,19 @@ export const PokemonCard = ({ pokemon }:Props) => {
           <Link href="/dashboard/main" className="px-4 py-2 hover:bg-gray-100 flex items-center">
 
               <div className="text-red-600">
-                <IoHeartOutline />
+                {
+                  isFavorite 
+                    ? (<IoHeart />)
+                    : (<IoHeartOutline />)
+                }
               </div>
               <div className="pl-3">
                 <p className="text-sm font-medium text-gray-800 leading-none">
-                  No es favorito
+                  {
+                    isFavorite
+                      ? 'Es favorito'
+                      : 'No es favorito'
+                  }
                 </p>
                 <p className="text-xs text-gray-500">View your campaigns</p>
               </div>
